a0f880362da09a8815b9e0d03012eaf7510458f8
Guy David Whittaker <105855731+gdwhittaker94@users.noreply.github.com>
2023-09-05 10:32:50 +0000

    Functioning todo list: adds items to list

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -2,21 +2,28 @@ import React, { useState } from 'react';
 
 const TodoList = () => {
     
-    const [input, setInput] = useState("")
+    const [todoList, setTodoList] = useState([]);       // List Array
+    const [itemValue, setItemValue] = useState("")      // <li> Item Content
 
-    const createTask = (input) => {
-        return <li>{input}</li>
+    const listUpdate = () => {
+        setTodoList([...todoList, itemValue]);          // Updates array with new "itemValue"
+        setItemValue("");                               // Resets input field 
     }
-
+    
     return (
         <>
-            <input type="text" onKeyDown={(event) => {if(event.key === "Enter") setInput(event.target.value) value={input}}}/>
-            <ul>
-                {createTask}
+            <input 
+                type="text" 
+                value={itemValue}                                                   // Controlled input: value controlled by React's state 
+                onChange={(event) => setItemValue(event.target.value)}              // Variable value in sync with input field value 
+                onKeyDown={(event) => {if(event.key === "Enter"){listUpdate()}}}    // Pressing Enter triggers function 
+            />
+            <ul>                                                                    
+                {todoList.map((item, index) => (<li key={index}>{item}</li>))}       
             </ul>
             <button>Borrar?</button>
         </>
-       
+        // <ul>: .map() to perform a function on each array item: create a <li> with each value inside todoList array.        
     )
 }
 
